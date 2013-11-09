40dfa9f73e43f50753b445a181d7c1ea40ef78d9
Yu Wang <wangyuhere@gmail.com>
2013-11-09 16:53:38 +0100

    add alias and bindkey and rails3 plugin

--- dotfiles/oh-my-zsh/custom/plugins/wangyuhere/wangyuhere.plugin.zsh
+++ dotfiles/oh-my-zsh/custom/plugins/wangyuhere/wangyuhere.plugin.zsh
@@ -6,6 +6,14 @@ export PATH="$(dirname $0)/bin:$PATH"
 
 alias hi-my-mac='st ~/Projects/hi-my-mac'
 alias p='cd ~/Projects'
+alias b="bundle"
+alias gi="gem install"
+alias migrate="rake db:migrate db:rollback && rake db:migrate db:test:prepare"
+alias s="rspec"
+
+bindkey "^A" beginning-of-line
+bindkey "^E" end-of-line
+bindkey "^P" history-search-backward
 
 function solr {
   cd ~/Projects/solr
