7420125b7af9fe5df67c47c46ada7a1e49f2a1f1
Yu Wang <wangyuhere@gmail.com>
2014-11-29 22:19:46 +0100

    Add p pe functions

--- dotfiles/oh-my-zsh/custom/plugins/wangyuhere/wangyuhere.plugin.zsh
+++ dotfiles/oh-my-zsh/custom/plugins/wangyuhere/wangyuhere.plugin.zsh
@@ -5,20 +5,22 @@ unsetopt correct_all
 export PATH="$(dirname $0)/bin:$PATH"
 
 alias hi-my-mac='st ~/Projects/hi-my-mac'
-alias p='cd ~/Projects'
 alias b="bundle"
 alias gi="gem install"
 alias migrate="rake db:migrate db:rollback && rake db:migrate db:test:prepare"
-alias s="rspec"
+alias s="bundle exec rspec"
 alias ss="bundle exec spring rspec"
 alias rpry="pry -r ./config/environment"
+alias t='smart_test'
 
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
 bindkey "^P" history-search-backward
 
-function solr {
-  cd ~/Projects/solr
-  ./start.sh $1
-  cd -
+function p {
+  cd ~/Projects/$1
+}
+
+function pe {
+  st ~/Projects/$1
 }
